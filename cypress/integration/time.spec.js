6bb82a38dee2f588136b6ddebb6c4276d22621a3
Renan Lopes <ree.lopes@hotmail.com>
2021-10-16 00:33:52 -0300

    Aula Inacabada

--- cypress/integration/time.spec.js
+++ cypress/integration/time.spec.js
@@ -14,11 +14,17 @@ describe('Work with alert', () => {
         // cy.get('#buttonNow').click()
         // cy.get('#resultado > span').should('contain', '31/12/1969')
         
-        const dt = new Date(2012, 3, 10, 15, 23, 50)
+        const dt = new Date(2012, 3, 10, 15, 23, 50) // yyyy, MM (inicia em 0), dd, hh, mm, ss
         cy.clock(dt.getTime())
         cy.get('#buttonNow').click()
         cy.get('#resultado > span').should('contain', '10/04/2012')
         
+    })
+
+    it('Goes to the future', () => {
+        cy.get('buttonTimePassed').click()
+        cy.get('#resultado > span').should('contain', '15731')
+        cy.get('#resultado > span').invoke('text').should('gt', 1573179785)
     })
 
 })
